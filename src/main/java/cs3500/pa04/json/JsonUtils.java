ba948494f3733caeb8c06e1c4c16f6804166ea75
drewhermes <hermes.a@northeastern.edu>
2023-06-08 19:23:00 -0400

    Changes by alpbb. Implemented JsonUtils and static serializeRecord method

---
+++ src/main/java/cs3500/pa04/json/JsonUtils.java
@@ -0,0 +1,20 @@
+package cs3500.pa04.json;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class JsonUtils {
+
+  /**
+   * Creates a JsonNode object from the given Record object
+   *
+   * @param record the JSON record to be converted into a JsonNode type object
+   * @return JsonNode representation of the given record
+   */
+  public static JsonNode serializeRecord(Record record) {
+    ObjectMapper objectMapper = new ObjectMapper();
+    return objectMapper.convertValue(record, JsonNode.class);
+
+  }
+
+}
